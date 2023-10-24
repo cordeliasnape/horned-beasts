ee724273378e3e5db69e90bc4cd7e20882e813a2
Cordelia Snape <snapecordelia@gmail.com>
2023-10-24 15:27:09 +0100

    complete lab for oct 24

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,9 +1,17 @@
-export default function HornedBeast({ title, img_url, description }) {
+import { useState } from "react";
+
+export default function HornedBeast({ title, img, description }) {
+  const [likes, setLikes] = useState(0);
+
+  function handleLikes() {
+    setLikes(likes + 1);
+  }
   return (
     <div className="animal">
       <h2>{title}</h2>
-      <img src={img_url} alt={title} />
+      <img src={img} alt={title} onClick={handleLikes} />
       <p>{description}</p>
+      <p>Favourites: {likes} ❤️</p>
     </div>
   );
 }
